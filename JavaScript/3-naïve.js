6f8af89f3a40e21d3eae150166c88e89a46c4223
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2024-03-04 13:40:06 +0200

    Naïve implementation

---
+++ JavaScript/3-naïve.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const signal = (value) => {
+  const box = { value };
+  const getter = () =>
+    typeof box.value === 'function' ? box.value() : box.value;
+  getter.set = (value) => (box.value = value);
+  getter.update = (callback) => (box.value = callback(box.value));
+  return getter;
+};
+
+const computed = (compute) => signal(compute);
+
+// Usage
+
+const count = signal(100);
+console.log(`Count 1: ${count()}`);
+
+count.set(200);
+console.log(`Count 2: ${count()}`);
+
+count.update((prev) => prev + 50);
+console.log(`Count 3: ${count()}`);
+
+const num = signal(1000);
+
+const total = computed(() => num() + count());
+console.log(`Total: ${total()}`);
